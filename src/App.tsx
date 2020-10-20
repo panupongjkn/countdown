8ff54b9ef87bec9157fb7053d17fbbe2281ecbe4
panupongjkn <panupong.jkn@gmail.com>
2020-10-20 23:29:07 +0700

    🎨 create form input date countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import 'antd/dist/antd.css';
 
 //pages
 import Index from "./pages/index";
